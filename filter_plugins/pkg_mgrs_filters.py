11923959acd9250ba7c2a4dc82452ac1dac21940
Markus Binsteiner <makkus@posteo.de>
2017-08-03 10:36:15 +0700

    Added check for 'auto' pkg_mgr

--- filter_plugins/pkg_mgrs_filters.py
+++ filter_plugins/pkg_mgrs_filters.py
@@ -16,4 +16,4 @@ class FilterModule(object):
 
     def pkg_mgr_executable_filter(self, pkg_mgrs):
 
-        return [PKG_MGRS_EXECUTABLES_MAP[pkg_mgr] for pkg_mgr in pkg_mgrs]
+        return [PKG_MGRS_EXECUTABLES_MAP[pkg_mgr] for pkg_mgr in pkg_mgrs if pkg_mgr != "auto"]
