eaf441e46e633d6a4d41215f99de7777a6b510d8
Markus Binsteiner <makkus@posteo.de>
2017-09-14 10:41:21 +0800

    Code cleanup, reformatting.

--- filter_plugins/pkg_mgrs_filters.py
+++ filter_plugins/pkg_mgrs_filters.py
@@ -8,13 +8,12 @@ PKG_MGRS_EXECUTABLES_MAP = {
     "vagrant_plugin": "vagrant"
 }
 
-class FilterModule(object):
 
+class FilterModule(object):
     def filters(self):
         return {
             'pkg_mgr_executable_filter': self.pkg_mgr_executable_filter
         }
 
     def pkg_mgr_executable_filter(self, pkg_mgrs):
-
         return [PKG_MGRS_EXECUTABLES_MAP[pkg_mgr] for pkg_mgr in pkg_mgrs if pkg_mgr != "auto"]
